414501729660e36148ed8d098d637f21662b938b
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-11-20 13:05:36 +0530

    Update Lru.java

--- OS/Lru.java
+++ OS/Lru.java
@@ -1,56 +1,73 @@
-import java.util.*;  
-    
-class Main{  
-    
-    Set<Integer> cache;  
-    int capacity;  
-    
-    public Main(int capacity)  
-    {  
-        this.cache = new LinkedHashSet<Integer>(capacity);  
-        this.capacity = capacity;  
-    }  
-public boolean get(int key)  
-    {  
-        if (!cache.contains(key))  
-            return false;  
-        cache.remove(key);  
-        cache.add(key);  
-        return true;  
-    }  
-    public void get_Value(int key)  
-    {          
-        if (get(key) == false)  
-           put(key);  
-    }  
-      
-    public void display()  
-    {  
-      LinkedList<Integer> list = new LinkedList<>(cache);  
-      Iterator<Integer> itr = list.descendingIterator();   
-          
-      while (itr.hasNext())  
-            System.out.print(itr.next() + " ");  
-    }  
-public void put(int key)  
-    {  
-            
-      if (cache.size() == capacity) {  
-            int firstKey = cache.iterator().next();  
-            cache.remove(firstKey);  
-        }  
-    
-        cache.add(key);  
-    }  
-    public static void main(String[] args)  
-    {  
-        Main obj = new Main(4);  
-        obj.get_Value(4);  
-        obj.get_Value(5);  
-        obj.get_Value(6);  
-        obj.get_Value(4);  
-        obj.get_Value(7);  
-        obj.get_Value(5);  
-        obj.display();  
-    }  
-}  
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int frames,pointer = 0, hit = 0, fault = 0,ref_len;
+        Boolean isFull = false;
+        int buffer[];
+        ArrayList<Integer> stack = new ArrayList<Integer>();
+        int reference[];
+
+        System.out.println("Please enter the number of Frames: ");
+        frames = Integer.parseInt(br.readLine());
+
+        System.out.println("Please enter the length of the Reference string: ");
+        ref_len = Integer.parseInt(br.readLine());
+
+        reference = new int[ref_len];
+        buffer = new int[frames];
+        for(int j = 0; j < frames; j++)
+            buffer[j] = -1;
+
+        System.out.println("Please enter the reference string: ");
+        for(int i = 0; i < ref_len; i++)
+        {
+            reference[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println();
+        for(int i = 0; i < ref_len; i++) {
+            if (stack.contains(reference[i])) {
+                stack.remove(stack.indexOf(reference[i]));
+            }
+            stack.add(reference[i]);
+            int search = -1;
+            for (int j = 0; j < frames; j++) {
+                if (buffer[j] == reference[i]) {
+                    search = j;
+                    hit++;
+                    break;
+                }
+            }
+            if (search == -1) {
+                if (isFull) {
+                    int min_loc = ref_len;
+                    for (int j = 0; j < frames; j++) {
+                        if (stack.contains(buffer[j])) {
+                            int temp = stack.indexOf(buffer[j]);
+                            if (temp < min_loc) {
+                                min_loc = temp;
+                                pointer = j;
+                            }
+                        }
+                    }
+                }
+                buffer[pointer] = reference[i];
+                fault++;
+                pointer++;
+                if (pointer == frames) {
+                    pointer = 0;
+                    isFull = true;
+                }
+            }
+
+        }
+        System.out.println("The number of Hits: " + hit);
+        System.out.println("Hit Ratio: " + (float)((float)hit/ref_len));
+        System.out.println("The number of Faults: " + fault);
+    }
+
+}
